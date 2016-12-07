f3a4a4c03d3431fcd59eac54d5a9a7a2a56679bc
Joe Marriott <marriott90@gmail.com>
2016-12-07 14:46:04 +0000

    updated list of students

--- directory.rb
+++ directory.rb
@@ -1,8 +1,10 @@
+#first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
 puts "Michael Corleone"
+puts "Nurse Ratched"
 puts "Alex DeLarge"
 puts "The Wicked Witch of the West"
 puts "Terminator"
@@ -10,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#finally, we print the total number of students
+print "Overall, we have "
+print 11
+puts " great students"
