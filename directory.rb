0753909dee25989b577c5d545f336e060b3b7585
Joe Marriott <marriott90@gmail.com>
2016-12-08 14:55:43 +0000

    aligned text output

--- directory.rb
+++ directory.rb
@@ -1,23 +1,31 @@
 def input_students
   students = [] # creates empty array
-  puts "Please enter the name of the student you are adding to the directory"
+  puts "Welcome to the student directory".center(80)
+  puts "Please enter the name of the student you are adding to the directory".center(80)
+  puts "....".center(80, '...')
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
 
-      puts "Please enter the country of birth of the student"
+      puts "Please enter the country of birth of the student".center(80, '...')
+      puts
       studentcountry = gets.chomp
 
-      puts "Please enter the height of the student in cm"
+      puts "Please enter the height of the student in cm".center(80, '...')
+      puts
       studentheight = gets.chomp
 
-      puts "Please enter a hobby of the student"
+      puts "Please enter a hobby of the student".center(80, '...')
+      puts
       studenthobby = gets.chomp
 
       students << {name: name, cohort: :november, country: studentcountry, hobby: studenthobby, height: studentheight }
-
-      puts "Now we have #{students.count} students"
-      puts "If you would like to add another student, type their name and press return, or just press return to continue"
+      puts
+      puts "Now we have #{students.count} students".center(80, '...')
+      puts
+      puts "If you would like to add another student, type their name and press return,".center(80)
+      puts "or just press return to continue".center(80)
+      puts ".................................".center(80, '...')
 
       name = gets.chomp # sets name value so loop runs again from the top
   end
@@ -31,16 +39,17 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(80)
+  puts "-------------".center(80)
 end
 
 def print(students)
   totalstudents = students.length # setting total number of loops
   counter = 0
   while counter < totalstudents # while loop to loop until all students outputted
-    puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort), Birthplace: #{students[counter][:country]}, Height: #{students[counter][:height]}cm, Hobby: #{students[counter][:hobby]}."
+    puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort), Birthplace: #{students[counter][:country]}, Height: #{students[counter][:height]}cm, Hobby: #{students[counter][:hobby]}.".center(80)
     counter += 1
+    puts
   end
 end
 
