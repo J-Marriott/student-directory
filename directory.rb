444e998ea403577060f482d9449d8a3e72c89d2d
Joe Marriott <marriott90@gmail.com>
2016-12-08 14:30:14 +0000

    allow user to input more information about a student when inputting

--- directory.rb
+++ directory.rb
@@ -1,20 +1,33 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # gets the first name
+  students = [] # creates empty array
+  puts "Please enter the name of the student you are adding to the directory"
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
+
+      puts "Please enter the country of birth of the student"
+      studentcountry = gets.chomp
+
+      puts "Please enter the height of the student in cm"
+      studentheight = gets.chomp
+
+      puts "Please enter a hobby of the student"
+      studenthobby = gets.chomp
+
+      students << {name: name, cohort: :november, country: studentcountry, hobby: studenthobby, height: studentheight }
+
+      puts "Now we have #{students.count} students"
+      puts "If you would like to add another student, type their name and press return, or just press return to continue"
+
+      name = gets.chomp # sets name value so loop runs again from the top
   end
+
+
+
   #return the array of students
   students
+
+
 end
 
 def print_header
@@ -26,7 +39,7 @@ def print(students)
   totalstudents = students.length # setting total number of loops
   counter = 0
   while counter < totalstudents # while loop to loop until all students outputted
-    puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+    puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort), Birthplace: #{students[counter][:country]}, Height: #{students[counter][:height]}cm, Hobby: #{students[counter][:hobby]}."
     counter += 1
   end
 end
